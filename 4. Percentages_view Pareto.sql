21095aaa67451b7bf2da713e001b0448d632c084
Santiago Acosta <a.antibohorquez@gmail.com>
2026-04-03 15:32:36 -0300

    4. Percentages_view Pareto.sql

--- 4. Percentages_view Pareto.sql
+++ 4. Percentages_view Pareto.sql
@@ -1,13 +1,20 @@
-CREATE OR REPLACE VIEW `pareto-12.paretodemo.Cumulative_percentages_view`
+-- Now we want to see
+-- What percentage of the revenue each Customer provided
+-- Which customers contributed to the 80% of sales
+-- We multiply by 100 and limit to 2 decimal places so we can see the percentages clearer
+CREATE OR REPLACE VIEW `pareto-12.paretodemo.Percentages_view`
 AS
 SELECT
-CustomerID, Customer_revenue,
- ROUND (Customer_revenue, 2) AS Customer_revenue_rounded,
-  ROUND (
-  SUM(Customer_revenue) OVER (ORDER BY Customer_revenue DESC
- ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW),
- 2) AS Cumulative_revenue,
- ROW_NUMBER() OVER (ORDER BY Customer_revenue DESC) AS Customer_Rank,
- COUNT(*) OVER () AS Total_customers,
- SUM(Customer_revenue) OVER () AS Total_revenue
+CustomerID,
+Customer_revenue,
+Cumulative_revenue,
+Customer_Rank,
+Total_customers,
+Total_revenue,
+ROUND (Cumulative_revenue / Total_revenue * 100, 2) AS Revenue_percentage,
+ROUND (Customer_Rank / Total_customers * 100, 2) AS Customer_percentage
 FROM `pareto-12.paretodemo.Cumulative_view`
+
+-- Now we can see what percetage of the revenue is being contributed by X percentage of the customers
+SELECT * 
+FROM `pareto-12.paretodemo.Percentages_view`
