1ab1012ad5336c7d6ebd6bacf74fa811c186167f
Santiago Acosta <a.antibohorquez@gmail.com>
2026-04-03 15:33:02 -0300

    5. Variable to prove Pareto.sql

--- 5. Variable to prove Pareto.sql
+++ 5. Variable to prove Pareto.sql
@@ -1,1 +1,13 @@
-sas
+-- Now we want to see
+-- Variable assignation to prove our 80/20 paleto method
+-- Set our variable to 80% of sales
+-- ADD ; after variable
+
+DECLARE Target_sales FLOAT64 DEFAULT 80;
+SELECT *
+FROM `pareto-12.paretodemo.Percentages_view`
+WHERE Revenue_percentage >= Target_sales
+LIMIT 1
+
+-- Now we know after that customer 1167 is where the remaining 20% is located
+-- And that 57% of our customers represent 80% of our sales
