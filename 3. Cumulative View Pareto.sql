e77a15efd9c8d816ee95d6e524ab8d7971a49240
Santiago Acosta <a.antibohorquez@gmail.com>
2026-04-03 15:31:49 -0300

    Update 3. Cumulative View Pareto.sql

--- 3. Cumulative View Pareto.sql
+++ 3. Cumulative View Pareto.sql
@@ -1,8 +1,21 @@
-CREATE OR REPLACE VIEW `pareto-12.paretodemo.Cumulativeview`
+-- Now we want to see
+-- CustomerID, Customer_revenue,revenue rounded, Rank of spending, Total of customers,
+-- Cumulative revenue and total revenue ALWAYS NEEDED TO GET % LATER
+-- Window options are important to keep the results on every ROW 
+
+CREATE OR REPLACE VIEW `pareto-12.paretodemo.Cumulative_view`
 AS
 SELECT
-CustomerID, Customer_revenue, ROW_NUMBER() OVER (ORDER BY Customer_revenue DESC) AS Cumulative_costumers
-FROM `pareto-12.paretodemo.CustomerID_revenue`
+ CustomerID,
+ Customer_revenue,
+  ROUND( 
+    SUM(Customer_revenue) OVER (ORDER BY Customer_revenue DESC
+    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW),2) AS Cumulative_revenue,
+ ROW_NUMBER() OVER (ORDER BY Customer_revenue DESC) AS Customer_Rank,
+ COUNT(*) OVER () AS Total_customers,
+ SUM(Customer_revenue) OVER () AS Total_revenue
+FROM `pareto-12.paretodemo.Customers_revenue_view`
 
+-- To see what we got out of it
 SELECT *
-FROM `pareto-12.paretodemo.Cumulativeview`
+FROM `pareto-12.paretodemo.Cumulative_view`
