0c6973a81324857b058bcb6dfe3643b6866d0e43
Santiago Acosta <a.antibohorquez@gmail.com>
2026-04-03 15:31:25 -0300

    2. Customers revenue View Pareto.sql

--- 2. Customers revenue View Pareto.sql
+++ 2. Customers revenue View Pareto.sql
@@ -1,13 +1,23 @@
-CREATE OR REPLACE VIEW `pareto-12.paretodemo.sales_v1`
+-- We create views to save our work 
+-- Now we group each Customer ID and the Total revenue gotten from them
+-- As well as where they are from
+-- Grouping by customer ID
+-- Ordering by revenue DESC
+
+CREATE OR REPLACE VIEW `pareto-12.paretodemo.Customers_revenue_view`
 AS
-SELECT CustomerID, SUM(Quantity * Unitprice) AS sales
+SELECT CustomerID,
+ROUND (SUM(Quantity * Unitprice),2) AS Customer_revenue,
+Country
 FROM `pareto-12.paretodemo.sales`
-GROUP BY CustomerID
+GROUP BY CustomerID, Country
+ORDER BY Customer_revenue DESC
 
 -- To see what we got out of it
 SELECT *
-FROM `pareto-12.paretodemo.sales_v1`
-LIMIT 20
+FROM pareto-12.paretodemo.Customers_revenue_view
+
+
 
 
 
